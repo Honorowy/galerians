567ebab1f3178b46ad1bb1bf92c94710bf5a0274
Nowe Imię <krzysztofcimoch@interia.pl>
2025-11-07 09:00:19 +0100

    Poczatkowe dzialanie

--- app/src/main/java/com/example/egzamin/MainActivity.java
+++ app/src/main/java/com/example/egzamin/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.egzamin;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +18,11 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private Button buttonLewo;
     private Button buttonPrawo;
+    private EditText editTextNumber;
+    private Switch switch1;
+    private ImageView imageView;
+    int[] obrazy = {R.drawable.luki, R.drawable.kamil, R.drawable.zdjecie1};
+    int indeks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,20 @@ public class MainActivity extends AppCompatActivity {
         });
         buttonLewo = findViewById(R.id.buttonLewo);
         buttonPrawo = findViewById(R.id.buttonPrawo);
+        editTextNumber = findViewById(R.id.editTextNumber);
+        switch1 = findViewById(R.id.switch1);
+        imageView = findViewById(R.id.imageView);
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+
+                } else {
+
+                }
+            }
+        });
 
         buttonLewo.setOnClickListener(new View.OnClickListener() {
             @Override
