59846f427efef4af3fde9f66701a8b0b2f13930b
Nowe Imię <krzysztofcimoch@interia.pl>
2025-11-07 08:19:24 +0100

    widok2+buttony

--- app/src/main/java/com/example/egzamin/MainActivity.java
+++ app/src/main/java/com/example/egzamin/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.egzamin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonLewo;
+    private Button buttonPrawo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        buttonLewo = findViewById(R.id.buttonLewo);
+        buttonPrawo = findViewById(R.id.buttonPrawo);
+
+        buttonLewo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        buttonPrawo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
         });
     }
 }
